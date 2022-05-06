537a70fba504663dd30e25dada4a60e40f84324c
r121212-Iwama <75057420+r121212-Iwama@users.noreply.github.com>
2022-05-06 13:27:09 +0900

    socket.io

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 const app  = express();
 const http = require("http").createServer(app);
+const io   = require("socket.io")(http);
 
+//-----------------------------------------------
+// グローバル変数
+//-----------------------------------------------
+// worksapceにあるすべてのブロックの情報(string)
+var worksapceBlock = "";
 
 app.use(express.static(__dirname + '/public/js'));
 /**
@@ -9,6 +15,27 @@ app.use(express.static(__dirname + '/public/js'));
 */
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/node.html");
+});
+
+/**
+ * [イベント] ユーザーが接続
+*/
+io.on("connection", (socket)=>{
+    console.log("ユーザーが接続しました");
+    console.log(worksapceBlock);
+    io.emit("workspace-post", worksapceBlock);
+
+    socket.on("workspace", (msg)=>{
+        if(worksapceBlock == msg){
+        return
+        }
+        worksapceBlock = msg;
+        console.log(worksapceBlock);
+        io.emit("workspace-post", worksapceBlock);
+        // console.log(msg);
+        // console.log(typeof(msg));
+    });
+
 });
 
 /**
